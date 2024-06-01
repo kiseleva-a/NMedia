9394242a666358eaff2706a306a6e6893425feef
Arina Kiseleva <arinaskiseleva@gmail.com>
2024-06-01 18:18:25 +0300

    muted unused file

--- app/src/main/java/ru/netology/nmedia/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -1,46 +1,46 @@
-package ru.netology.nmedia.db
-
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import ru.netology.nmedia.dao.PostDao
-import ru.netology.nmedia.entity.PostEntity
-
-
-@Database(entities = [PostEntity::class], version = 1)
-abstract class AppDb : RoomDatabase() {
-    abstract fun postDao(): PostDao
-
-    companion object {
-        @Volatile
-        private var instance: AppDb? = null
-
-        fun getInstance(context: Context): AppDb {
-            return instance ?: synchronized(this) {
-                instance ?: buildDatabase(context).also { instance = it }
-            }
-        }
-
-        private fun buildDatabase(context: Context) =
-            Room.databaseBuilder(context,AppDb::class.java, "app.db")
-                .allowMainThreadQueries()
-                .build()
-    }
-}
-
-//class DbHelper(context: Context, dbVersion: Int, dbName: String, private val DDLs: Array<String>) :
-//    SQLiteOpenHelper(context, dbName, null, dbVersion) {
-//    override fun onCreate(db: SQLiteDatabase) {
-//        DDLs.forEach {
-//            db.execSQL(it)
+//package ru.netology.nmedia.db
+//
+//import android.content.Context
+//import androidx.room.Database
+//import androidx.room.Room
+//import androidx.room.RoomDatabase
+//import ru.netology.nmedia.dao.PostDao
+//import ru.netology.nmedia.entity.PostEntity
+//
+//
+//@Database(entities = [PostEntity::class], version = 1)
+//abstract class AppDb : RoomDatabase() {
+//    abstract fun postDao(): PostDao
+//
+//    companion object {
+//        @Volatile
+//        private var instance: AppDb? = null
+//
+//        fun getInstance(context: Context): AppDb {
+//            return instance ?: synchronized(this) {
+//                instance ?: buildDatabase(context).also { instance = it }
+//            }
 //        }
-//    }
 //
-//    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-//        TODO("Not implemented")
+//        private fun buildDatabase(context: Context) =
+//            Room.databaseBuilder(context,AppDb::class.java, "app.db")
+//                .allowMainThreadQueries()
+//                .build()
 //    }
+//}
 //
-//    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-//        TODO("Not implemented")
-//    }
+////class DbHelper(context: Context, dbVersion: Int, dbName: String, private val DDLs: Array<String>) :
+////    SQLiteOpenHelper(context, dbName, null, dbVersion) {
+////    override fun onCreate(db: SQLiteDatabase) {
+////        DDLs.forEach {
+////            db.execSQL(it)
+////        }
+////    }
+////
+////    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+////        TODO("Not implemented")
+////    }
+////
+////    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+////        TODO("Not implemented")
+////    }
