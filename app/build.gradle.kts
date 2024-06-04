17bcbdba920acba10c4f1712c004fe4e7657bf67
Arina Kiseleva <arinaskiseleva@gmail.com>
2024-06-05 02:40:27 +0300

    added buildconfig again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.ir.backend.js.compile
-
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -14,6 +11,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 
     defaultConfig {
@@ -80,7 +78,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation ("androidx.room:room-ktx:2.6.1")
 
 }
